73599660705233ddfd5614dda6c48b6cbc6f0e16
yashwant162 <yashwantsingh162@gmail.com>
2023-10-31 16:23:05 +0530

    add constants.js, update packages

---
+++ server/constants.js
@@ -0,0 +1,12 @@
+exports.allowedHosts = {
+  credentials: true,
+  origin: "http://127.0.0.1:5173",
+};
+
+exports.constants = {
+  VALIDATION_ERROR: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+};
