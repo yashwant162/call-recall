335636ff610e474ef71ea22ca70843770aeeadc7
yashwant162 <yashwantsingh162@gmail.com>
2023-11-01 16:04:22 +0530

    add routes and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/homepage/HomePage";
 
+axios.defaults.baseURL = "http://127.0.0.1:5000";
+axios.defaults.withCredentials = true;
 function App() {
-
   return (
-    <>
-      <div className='text-slate-600 text-4xl font-bold'>Call Recall</div>
-    </>
-  )
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+            <Route index element={<HomePage/>} />
+        </Route>
+      </Routes>
+  );
 }
 
-export default App
+export default App;
