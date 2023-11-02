0b3a5fcbbb00a845e5255177c43d56b3d1e1c0d3
yashwant162 <yashwantsingh162@gmail.com>
2023-11-02 10:24:47 +0530

    add summary api in home page

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -5,11 +5,13 @@ import axios from "axios";
 export default function HomePage() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [generatedText, setGeneratedText] = useState("")
-  const [loading, setLoading] = useState(false);
+  const [summarizedText, setSummarizedText] = useState("")
+  const [loading1, setLoading1] = useState(false);
+  const [loading2, setLoading2] = useState(false);
 
   const onDrop = useCallback(async (acceptedFiles) => {
     const file = acceptedFiles[0];
-    
+    setSummarizedText("")
     if (file) {
       setSelectedFile(file);
       const formData = new FormData();
@@ -20,16 +22,30 @@ export default function HomePage() {
       } catch(error) {
         console.error("File upload error", error);
       }
-      setLoading(true)
+      setLoading1(true)
       const {data} = await axios.post("http://localhost:5000/api/data/convert-to-text",fileName)
       const text = data.text
       console.log(text)
       setGeneratedText(text)
-      setLoading(false)
+      setLoading1(false)
     }
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({onDrop,accept: "audio/*"});
+
+  const summarizeText = async () => {
+    const sendData = {
+      text: generatedText
+    }
+    // const sendData = {
+    //   text: "Say hello"
+    // }
+    setLoading2(true)
+    const {data} = await axios.post("http://localhost:5000/api/data/summarize-text", sendData)
+    console.log(data.text)
+    setSummarizedText(data.text)
+    setLoading2(false)
+  }
 
   return (
     <div className="h-full mb-4 mt-4 w-full">
@@ -61,15 +77,18 @@ export default function HomePage() {
             className="w-2/5 h-[70vh] p-2 border border-gray-300 rounded-lg mb-4 resize-none"
             placeholder="Speech generated text will come here"
             readOnly
-            value={loading ? 'Generating Text...' : generatedText}
+            value={loading1 ? 'Generating Text...' : generatedText}
           ></textarea>
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg ml-4">
+          {generatedText && (<button className="bg-blue-500 text-white py-2 px-4 rounded-lg ml-4"
+            onClick={summarizeText}
+          >
             Convert
-          </button>
+          </button>)}
           <textarea
             className="w-2/5 h-[70vh] p-2 border border-gray-300 rounded-lg resize-none"
             placeholder="Summarized text will be displayed here"
             readOnly
+            value={loading2 ? 'Summarizing Text...' : summarizedText}
           ></textarea>
         </div>
       </div>
