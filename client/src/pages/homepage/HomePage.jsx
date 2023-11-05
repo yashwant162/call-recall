154d0ed88ced80ebd7d8817876555e7be9f40376
yashwant162 <yashwantsingh162@gmail.com>
2023-11-05 13:45:15 +0530

    add record your voice feature using react-media-recorder

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -1,26 +1,30 @@
 /* eslint-disable no-unused-vars */
 import { useCallback, useEffect, useState, useRef } from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
 import UploadLogo from "../../components/svg/UploadLogo";
 import LoadingSpinner from "../../components/svg/LoadingSpinner";
 import LoadingDots from "../../components/svg/LoadingDots";
 import Spinner from "../../components/svg/Spinner";
+import MicIcon from "../../components/svg/MicIcon";
+import CopyIcon from "../../components/svg/CopyIcon";
 
 export default function HomePage() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [generatedText, setGeneratedText] = useState("");
   const [summarizedText, setSummarizedText] = useState("");
   const [isfilePresent, setIsFilePresent] = useState(false);
+  const [audioBlob, setAudioBlob] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
   const [text1, setText1] = useState("");
   const [text2, setText2] = useState("");
   const [loading1, setLoading1] = useState(false);
   const [loading2, setLoading2] = useState(false);
-
   useEffect(() => {
-    let currentIndex = text1.length;
-    if (!loading1 && currentIndex < generatedText.length) {
-      const timer = setTimeout(() => {
+        let currentIndex = text1.length;
+        if (!loading1 && currentIndex < generatedText.length) {
+            const timer = setTimeout(() => {
         setText1(generatedText.slice(0, currentIndex + 1));
       }, 40);
 
@@ -38,34 +42,92 @@ export default function HomePage() {
       return () => clearTimeout(timer);
     }
   }, [text2, summarizedText, loading2]);
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({
+      audio: true,
+      onStop: async (blobUrl) => {
+        console.log(status);
+        setGeneratedText("");
+        setAudioBlob(blobUrl);
+        stopRecording();
+        const response = await fetch(blobUrl);
+        const blob = await response.blob();
+        const formData = new FormData();
+        formData.append("audioFile", blob, "uploaded_audio.mp4");
+        console.log("sending audio");
+        const fileName = await uploadAudio(formData);
+        console.log("using record", fileName);
+        setLoading1(true);
+        setText1("");
+        const text = await getGeneratedText(fileName);
+        console.log("text received", text);
+        if (text === null) {
+          console.log("text === null");
+          setLoading1(false);
+          setSelectedFile(null);
+          return;
+        }
+        setGeneratedText(text);
+        setLoading1(false);
+      },
+    });
+
+  const uploadAudio = async (formData) => {
+    try {
+      var { data } = await axios.post(
+        "http://localhost:5000/api/data/upload-audio",
+        formData
+      );
+      console.log("File uploaded successfully", data.fileUrl);
+      return data;
+    } catch (error) {
+      console.error("File upload error", error);
+      return null;
+    }
+  };
+
+  const getGeneratedText = async (fileName) => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/data/convert-to-text",
+        fileName
+      );
+      console.log("received text", data.text);
+      return data.text;
+    } catch (err) {
+      console.log(`Text could not be generated ${err}`);
+      return null;
+    }
+  };
 
   const onDrop = useCallback(async (acceptedFiles) => {
     const file = acceptedFiles[0];
     setSummarizedText("");
+    setSelectedFile(null);
     if (file) {
       setSelectedFile(file);
       setIsFilePresent(true);
       const formData = new FormData();
       formData.append("audioFile", file);
-      try {
-        var fileName = await axios.post(
-          "http://localhost:5000/api/data/upload-audio",
-          formData
-        );
-        console.log("File uploaded successfully", fileName);
-      } catch (error) {
-        console.error("File upload error", error);
+
+      var fileName = await uploadAudio(formData);
+      if (fileName === null) {
+        setSelectedFile(null);
+        setIsFilePresent(false);
         return;
       }
+      console.log("file name received using dropbox", fileName);
       await new Promise((resolve) => setTimeout(resolve, 1000));
       setIsFilePresent(false);
       setLoading1(true);
-      const { data } = await axios.post(
-        "http://localhost:5000/api/data/convert-to-text",
-        fileName
-      );
-      const text = data.text;
+      setText1("");
+      const text = await getGeneratedText(fileName);
       console.log(text);
+      if (text === null) {
+        setLoading1(false);
+        setSelectedFile(null);
+        return;
+      }
       setGeneratedText(text);
       setLoading1(false);
     }
@@ -98,11 +160,22 @@ export default function HomePage() {
         sendData
       );
       console.log(data.text);
+      setText2("");
       setSummarizedText(data.text);
     } catch (err) {
       console.log(err);
     }
     setLoading2(false);
+  };
+
+  const toggleRecording = () => {
+    if (isRecording) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+
+    setIsRecording(!isRecording);
   };
 
   return (
@@ -162,19 +235,27 @@ export default function HomePage() {
           <div className=" inline-flex items-center justify-center text-gray-500 font-semibold tracking-wider text-md lg:w-40 lg:h-32">
             OR
           </div>
-          <div className="flex flex-row items-center tracking-wider lg:w-96 lg:h-32">
+          <div className="flex flex-row items-center tracking-wider lg:w-96 lg:h-32 lg:gap-5 text-red-400">
             <button
-              onClick
-              className={` transition-all ease-in duration-75 bg-gray-900 rounded-md  hover:text-gray-400 
-                 text-gray-600 text-md tracking-wider font-semibold border border-gray-600 hover:border-fourth
-                  relative inline-flex items-center justify-center px-2 py-2`}
+              onClick={toggleRecording}
+              className={` text-red-500 border border-fourth hover:bg-red-600 hover:text-white 
+                 font-medium rounded-full text-sm px-6 py-6 text-center inline-flex 
+                 items-center ${
+                   isRecording &&
+                   "border-green-700 hover:bg-green-400 text-green-400"
+                 } `}
             >
-              Start Recording
+              <MicIcon />
             </button>
+            <span
+              className={`${isRecording ? "text-green-400" : "text-red-500"}`}
+            >
+              {isRecording ? "Recording" : "Record your voice"}
+            </span>
           </div>
         </div>
         <div className="lg:pl-20 lg:pr-20 lg:flex lg:flex-row lg:items-center lg:justify-between lg:mt-4 sm:flex sm:flex-col sm:justify-center sm:items-center">
-          <div className="w-2/5 h-[50vh] border border-fourth p-2 rounded-lg bg-primary bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
+          <div className="relative w-2/5 h-[50vh] border border-fourth p-2 rounded-lg bg-primary bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
             {loading1 ? (
               <LoadingDots />
             ) : (
@@ -185,6 +266,7 @@ export default function HomePage() {
                 value={text1}
               />
             )}
+            {/* <CopyIcon /> */}
           </div>
 
           <button
